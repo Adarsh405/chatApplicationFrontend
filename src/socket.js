236f6cb59026cff312c517f991bca4582acb9ecc
Adarsh <125676861+Adarsh405@users.noreply.github.com>
2026-09-15 00:24:16 +0530

    Update socket connection to use environment variable

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
-  autoConnect: true,
-});
+const socket = io(
+  import.meta.env.VITE_API_URL,
+  {
+    autoConnect: true,
+    transports: ["websocket", "polling"],
+  }
+);
 
 export default socket;
